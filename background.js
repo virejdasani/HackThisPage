6910c158b9d17d31ae7397573836e1ec7f95ecde
Virej Dasani <dasanivirej@gmail.com>
2021-03-01 12:25:39 +0530

    It now works when the extension is clicked instead of it working without clicking the extension

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
-// This file runs in the background to detect when a new tab is opened where the code in content.js can be injected
-chrome.browserAction.onClicked.addListener(
-    function (tab) {
-        chrome.tabs.executeScript(tab.id, {
-            "file": "content.js"
-        });
-    });
+// This runs in the background
+console.log("Background script active!")
+
+// This is the onclick listener for clicking the extension button in the extensions bar
+chrome.browserAction.onClicked.addListener(buttonClicked)
+
+// This is executed when the extension is clicked in the extension bar
+function buttonClicked(tab) {
+    console.log("Button Clicked")
+    console.log(tab)
+
+
+    let msg = {
+        txt: "Clicked"
+    }
+    // This is to send a message to the content script
+    // It sends the MSG below to the active tab (tab.id)
+    chrome.tabs.sendMessage(tab.id, msg)
+    // This message is received in the content script
+}
--- content.js
+++ content.js
@@ -1,2 +1,10 @@
-// This makes webpage editable
-document.body.contentEditable = 'true'
+// This receives the message from the background script 
+
+chrome.runtime.onMessage.addListener(gotMessage)
+
+function gotMessage(message, sender, sendResponse) {
+    console.log(message.txt)
+    if (message.txt === "Clicked") {
+        document.body.contentEditable = 'true'
+    }
+}
