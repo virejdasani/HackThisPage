4a9a97da5e5c16e80f308914d19bc6fda7c91e40
Virej Dasani <dasanivirej@gmail.com>
2021-03-01 15:01:30 +0530

    EVERYTHING WORKS, Finally

--- content.js
+++ content.js
@@ -1,6 +1,10 @@
 // Here the message is received from popup.js 
 chrome.runtime.onMessage.addListener(msgObj => {
     if (msgObj === 'Clicked') {
-        console.log("CLICKSED")
+        console.log("HACK BUTTON CLICKED")
+
+        // Make the webpage editable (HACKED)
+        document.body.contentEditable = 'true'
+        console.log("WEBSITE HACKED 🚀")
     }
 });
