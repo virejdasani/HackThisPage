8f213a1c0d0748fa69e989a5368a22cebb02180a
Virej Dasani <dasanivirej@gmail.com>
2021-03-01 13:39:09 +0530

    Added comments

--- content.js
+++ content.js
@@ -4,6 +4,7 @@ chrome.runtime.onMessage.addListener(gotMessage)
 
 function gotMessage(message, sender, sendResponse) {
     console.log(message.txt)
+    // If the extension has been clicked, this if statement is executed
     if (message.txt === "Clicked") {
         document.body.contentEditable = 'true'
     }
