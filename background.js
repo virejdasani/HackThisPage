28f0c0dffa82050d06162e5b23e2f98343e2c6db
Virej Dasani <dasanivirej@gmail.com>
2021-03-01 14:53:23 +0530

    It can now finally send a message from popup.js to content.js

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
-// This runs in the background
-console.log("Background script active!")
+// // This runs in the background
+// console.log("Background script active!")
 
-// This is the onclick listener for clicking the extension button in the extensions bar
-chrome.browserAction.onClicked.addListener(buttonClicked)
+// // This is the onclick listener for clicking the extension button in the extensions bar
+// chrome.browserAction.onClicked.addListener(buttonClicked)
 
-// This is executed when the extension is clicked in the extension bar
-function buttonClicked(tab) {
-    console.log("Button Clicked")
-    console.log(tab)
+// // This is executed when the extension is clicked in the extension bar
+// function buttonClicked(tab) {
+//     console.log("Button Clicked")
+//     console.log(tab)
 
 
-    let msg = {
-        txt: "Clicked"
-    }
-    // This is to send a message to the content script
-    // It sends the MSG below to the active tab (tab.id)
-    chrome.tabs.sendMessage(tab.id, msg)
-    // This message is received in the content script
-}
+//     let msg = {
+//         txt: "Clicked"
+//     }
+//     // This is to send a message to the content script
+//     // It sends the MSG below to the active tab (tab.id)
+//     chrome.tabs.sendMessage(tab.id, msg)
+//     // This message is received in the content script
+// }
