4e99bf0d6910cebcaf09adef11c205c8e5001431
Virej Dasani <dasanivirej@gmail.com>
2021-03-10 09:09:34 +0530

    Addded new images and set spellcheck to false for the document body

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// Here the message is received from popup.js 
+// Here the message is received from popup.js
 chrome.runtime.onMessage.addListener(msgObj => {
     if (msgObj === 'Clicked') {
 
@@ -7,6 +7,10 @@ chrome.runtime.onMessage.addListener(msgObj => {
         // Make the webpage editable (HACKED)
         document.body.contentEditable = 'true'
         console.log("WEBSITE HACKED 🚀")
+
+        // This turns off spellcheck
+        document.body.setAttribute('spellcheck', false)
+        
 
         // console.log("This tool has been created by @VirejDasani")
     }
