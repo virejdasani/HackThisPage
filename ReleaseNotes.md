d11f347a128c8527da0b6ae7b6a6f81dcab7f13d
VirejDasani <dasanivirej@gmail.com>
2024-06-08 23:22:55 +0100

    Hack This Page 2 🎉

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,8 +1,16 @@
 # v1.0
+
 - This is the first version of Hack This Page
 
 # v2.0
+
 - Now there will be no spellcheck in the webpage to avoid red underlines for spelling not in the dictionary
 
 # v3.0
+
 - Added an alert on the website that has been hacked to notify the user that the hack was successful, added a 'Leave a review' link under tutorial link and added google analytics
+
+# v0.2
+
+- Hack this page is back after being sold to a company overseas.
+- Better UI, bug fixes, manifest v3
--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
-var msgObj = "Clicked"
+var msgObj = "Clicked";
 
 // This is executed when the hack button is clicked
-document.getElementById('hackButton').addEventListener('click', function () {
-    // This sends a message to content.js from where it receives it
-    chrome.tabs.query({}, tabs => {
-        tabs.forEach(tab => {
-            chrome.tabs.sendMessage(tab.id, msgObj);
-        })
-    })
-})
+document.getElementById("hackButton").addEventListener("click", function () {
+  // This sends a message to content.js from where it receives it
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      chrome.tabs.sendMessage(tab.id, msgObj);
+    });
+  });
+});
